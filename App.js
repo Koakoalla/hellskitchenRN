50dfa8a2291e9da64ac19654bad67dbcfca70d59
Koakoalla <iraazazaza@MacBook-Air-Koala.local>
2022-11-08 22:33:25 +0300

    v2

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+
+//Screens
+import ProductContainer from './Screens/Products/ProductContainer'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <ProductContainer/>
     </View>
   );
 }
@@ -13,7 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    Color: '#32a852',
+
+    backgroundColor: '#32a852',
     alignItems: 'center',
     justifyContent: 'center',
   },
